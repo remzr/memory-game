dab968a204fe1fa9ce6ef57201a8af36a84adaba
remzr <110097561+remzr@users.noreply.github.com>
2025-12-29 20:07:06 +0100

    Create hook for characters (cards)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState[null];
+
+  //Fetch character data on App load and save it to characters hook
+  
 
   return (
     <>
