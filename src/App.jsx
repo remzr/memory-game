7a5da4113f5b6b86217e60324512d1a96f90ca56
remzr <110097561+remzr@users.noreply.github.com>
2025-12-30 14:09:55 +0100

    Setup fetch call to public API an save data in hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [characters, setCharacters] = useState([null]);
+  const [characters, setCharacters] = useState(null);
   const [cardTitle, setCardTitle] = useState(null);
   const [imageSrc, setImageSrc] = useState(null);
+  const [error, setError] = useState(null);  
 
-  //Fetch character data on App load and save it to characters hook
+  //Fetch data and cache it in const
+  const characterData = () => {
+    return fetch("https://pokeapi.co/api/v2/ability/?limit=20&offset=20")
+      .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP-Error: ${response.status}`);
+      }
+        return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => { 
+      console.log("Fetch error:", error);
+      throw error;
+    });
+  }
+  
+  //Assigne fetched data to character hook
+  useEffect(() => {
+    characterData()
+      .then((data) => {
+        console.log(data.results);
+        setCharacters(data.results);
+      })
+      .catch(setError);
+  }, []);
 
   return (
     <>
